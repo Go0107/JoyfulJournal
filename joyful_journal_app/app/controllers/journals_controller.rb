120ff1a653980a617ed697cc8995b3566484bbb4
Go0107 <srfiveu.0107@gmail.com>
2024-05-19 03:16:16 +0900

    日記投稿、一覧ページの実装

--- joyful_journal_app/app/controllers/journals_controller.rb
+++ joyful_journal_app/app/controllers/journals_controller.rb
@@ -1,7 +1,18 @@
 class JournalsController < ApplicationController
   def index
+    @journals = Journal.all
   end
 
   def new
+  end
+
+  def create
+    @journal = Journal.new(content: params[:content], user_id: current_user.id)
+    if @journal.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/journals")
+    else
+      render("/journals/new")
+    end
   end
 end
--- joyful_journal_app/db/schema.rb
+++ joyful_journal_app/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_16_195205) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_18_173518) do
   create_table "journals", force: :cascade do |t|
     t.text "content"
     t.string "stamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
