9bb8eae3f9f597f7563019d4817ff6f82ea24519
Go0107 <srfiveu.0107@gmail.com>
2024-06-12 20:50:42 +0900

    スタンプ機能の作成

--- joyful_journal_app/app/controllers/journals_controller.rb
+++ joyful_journal_app/app/controllers/journals_controller.rb
@@ -4,10 +4,15 @@ class JournalsController < ApplicationController
   end
 
   def new
+    @journal = Journal.new
   end
 
   def create
-    @journal = Journal.new(content: params[:content], user_id: current_user.id)
+    @journal = Journal.new(
+      content: params[:content], 
+      user_id: current_user.id,
+      stamp: params[:stamp],
+    )
     if @journal.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/journals")
